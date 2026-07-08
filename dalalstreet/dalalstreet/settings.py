33bf088d31fa483e9f793c847cb5acc9462fb57f
Inderjot Singh <singhinderjot816@gmail.com>
2026-07-08 21:14:34 +0530

    settings.py

--- dalalstreet/dalalstreet/settings.py
+++ dalalstreet/dalalstreet/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
