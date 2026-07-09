5e6a0248cb434b318ae31a2cae29816b436b6a2f
Inderjot Singh <singhinderjot816@gmail.com>
2026-07-10 02:37:22 +0530

    changes

--- dalalstreet/dalalstreet/settings.py
+++ dalalstreet/dalalstreet/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["*"]
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = [
+    "dalal-street-ai.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
